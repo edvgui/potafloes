fe0167af0588c200a780ca32609505e55b296ec8
Guillaume Everarts de Velp <edvgui@gmail.com>
2022-09-15 00:18:18 +0200

    Add a few assertions

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import logging
+
+from potafloes.entity_context import EntityContext
 logging.basicConfig(level=logging.DEBUG)
 
 from potafloes.attachments import bag
@@ -30,14 +32,12 @@ class Test(Person):
     pass
 
 
-@implementation(Test)
 @implementation(Person)
 async def handle_children(person: Person) -> None:
     print(f"Dealing with children of {person}")
     person.children.subscribe(callback=person.praise_child)
 
 
-@implementation(Test)
 @implementation(Person)
 async def handle_parents(person: Person) -> None:
     print(f"Dealing with parents of {person}")
@@ -47,7 +47,7 @@ async def handle_parents(person: Person) -> None:
     person.parents.subscribe(callback=praise_parent)
 
 
-Person.parents < exchange() > Person.children  # TODO support inheritance
+Person.parents < exchange() > Person.children
 
 
 async def main() -> None:
@@ -59,15 +59,32 @@ async def main() -> None:
 
     marilyn.parents += bob
 
-    alice = Test(name="alice", likes_dogs=False, parents=marilyn.parents)
+    Test(name="alice", likes_dogs=False, parents=marilyn.parents)
 
     bob_2 = await Person.get(index=Person.unique_name, arg="bob")
-    print(bob == bob_2)
-    print(bob == await will_be_bob)
-
+    assert bob == bob_2
+    assert bob == await will_be_bob
 
 Context().run(main)
+
+person_context = EntityContext.get(entity_type=Person)
+test_context = EntityContext.get(entity_type=Test)
+
+bob = person_context.find_instance(query=Person.unique_name, result="bob")
+assert bob.name == "bob"
+
+alice = test_context.find_instance(query=Person.unique_name, result="alice")
+assert alice.name == "alice"
+
+marilyn = person_context.find_instance(query=Person.unique_name, result="marilyn")
+assert marilyn.name == "marilyn"
+
+assert alice in bob.children._all()
+assert marilyn in bob.children._all()
+assert bob in alice.parents._all()
+assert bob in marilyn.parents._all()
+
+assert Test.parents is Person.parents
+
 Context().reset()
 Context().run(main)
-
-print(Person.children)
